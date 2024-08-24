925fa4bc533b0167f19b8700210e940e477fc5b8
asahelcode <hoyx0101@gmail.com>
2024-08-24 11:49:23 +0100

    implement zustand for state management

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import MapView, { PROVIDER_DEFAULT } from 'react-native-maps'
+import { useLocationStore } from '@/store'
 
 const Map = () => {
+  const { userLongitude, userLatitude, destinationLatitude, destinationLongitude } = useLocationStore();
+
+  
   return (
-    <MapView provider={PROVIDER_DEFAULT} className='w-full h-full rounded-2xl' tintColor='black' mapType='mutedStandard' showsPointsOfInterest={false}
+    <MapView 
+    provider={PROVIDER_DEFAULT} 
+    className='w-full h-full rounded-2xl' 
+    tintColor='black' 
+    mapType='mutedStandard' 
+    showsPointsOfInterest={false}
     showsUserLocation={true}
     userInterfaceStyle='light'
-    ></MapView>
+    >
+
+    </MapView>
   )
 }
 
